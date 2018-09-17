b1f0c43e3ad8de37bb34893840db303070b40fb8
MAX <MAX@DESKTOP-3CIBT8S>
2018-09-17 19:35:39 +0300

    Lab

--- Lab1/lab1.cpp
+++ Lab1/lab1.cpp
@@ -1,5 +1,13 @@
 //Полінка Максим, К-24
 
+/*
+TODO:
+(1)Ask Onotsky about how to take a derivative
+(2)
+(3)
+
+*/
+
 //storing the labs in the Git repository...
 //can't store in a Git Repository because the path of this one is ./LR1/...
 //and the path of all the others are in ./Second_course_projects
@@ -14,107 +22,110 @@ using namespace std;
 
 typedef void (Rand_gen::*func)(void);
 
-	Rand_gen::Rand_gen()//ask why this isn't working ???
-	{
-		methods[0] = &Rand_gen::meth01;
-		methods[1] = &Rand_gen::meth02;
-		methods[2] = &Rand_gen::meth03;
-		methods[3] = &Rand_gen::meth04;
-		methods[4] = &Rand_gen::meth05;
-		methods[5] = &Rand_gen::meth06;
-		methods[6] = &Rand_gen::meth07;
-		methods[7] = &Rand_gen::meth08;
-		methods[8] = &Rand_gen::meth09;
-		methods[9] = &Rand_gen::meth10;
+Rand_gen::Rand_gen()//ask why this isn't working ???
+{
+	methods[0] = &Rand_gen::meth01;
+	methods[1] = &Rand_gen::meth02;
+	methods[2] = &Rand_gen::meth03;
+	methods[3] = &Rand_gen::meth04;
+	methods[4] = &Rand_gen::meth05;
+	methods[5] = &Rand_gen::meth06;
+	methods[6] = &Rand_gen::meth07;
+	methods[7] = &Rand_gen::meth08;
+	methods[8] = &Rand_gen::meth09;
+	methods[9] = &Rand_gen::meth10;
+}
 
-		
-	}
-
-	void Rand_gen::meth01()
+void Rand_gen::meth01()
+{
+	int x0 = 1, a = 23, b = 31, M = 71, x;
+	double y;
+	for (int k = 0; k < 10; k++)
 	{
-		int x0 = 1, a = 23, b = 31, M = 71, x;
-		double y;
+		x = (a * x0 + b) % M;
+		y = (double)x / M;
 		for (int k = 0; k < 10; k++)
 		{
-			x = (a * x0 + b) % M;
-			y = (double)x / M;
-			for (int k = 0; k < 10; k++)
-			{
-				if (y >= (double)k / 10 && y < (double)(k + 1) / 10) c[k]++;
-				x0 = x;
-			}
+			if (y >= (double)k / 10 && y < (double)(k + 1) / 10) c[k]++;
+			x0 = x;
 		}
 	}
+}
 
-	void Rand_gen::meth02()
+void Rand_gen::meth02()
+{
+	cout << "Method02" << endl;
+}
+
+void Rand_gen::meth03()
+{
+	cout << "Method03" << endl;
+	//mod M - is % M
+	//int x_prev, x, x_next;//initialize
+	//for (int k = 0; k < 10; k++)
+	//{
+
+	//}
+}
+
+void Rand_gen::meth04()//(*)
+{
+	cout << "Method04" << endl;
+}
+
+void Rand_gen::meth05()
+{
+	cout << "Method05" << endl;
+}
+
+void Rand_gen::meth06()
+{
+	cout << "Method06" << endl;
+}
+
+void Rand_gen::meth07()
+{
+	cout << "Method07" << endl;
+}
+
+void Rand_gen::meth08()
+{
+	cout << "Method08" << endl;
+}
+
+void Rand_gen::meth09()
+{
+	cout << "Method09" << endl;
+}
+
+void Rand_gen::meth10()
+{
+	cout << "Method10" << endl;
+}
+
+int Rand_gen::menu(void)
+{
+	int func_num;
+	cout << "Please, enter the number of the method: " << endl;
+	cin >> func_num;
+	return func_num;
+}
+
+void Rand_gen::doit(int k)
+{
+	func p = this->methods[k - 1];
+	(this->*p)();
+}
+
+int main()
+{
+	int k;
+	Rand_gen obj;
+	do
 	{
-		int x, c;
-		//ask Onotsky about how to take a derivative 
-	}
-
-	void Rand_gen::meth03()
-	{
-		//mod M - is % M
-		int x_prev, x, x_next;//initialize
-		for (int k = 0; k < 10; k++)
-		{
-
-		}
-	}
-
-	void Rand_gen::meth04()//(*)
-	{
-
-	}
-
-	void Rand_gen::meth05()
-	{
-
-	}
-
-	void Rand_gen::meth06()
-	{
-
-	}
-
-	void Rand_gen::meth07()
-	{
-
-	}
-
-	void Rand_gen::meth08()
-	{
-
-	}
-
-	void Rand_gen::meth09()
-	{
-
-	}
-
-	void Rand_gen::meth10()
-	{
-
-	}
-
-	int Rand_gen::menu(void)
-	{
-		int func_num;
-		cout << "Please, enter the number of the method: " << endl;
-		cin >> func_num;
-		return func_num;
-	}
-
-	int main()
-	{
-		int k;
-		Rand_gen obj;
-		do
-		{
-			k = obj.menu();
-			if (k == 0) break;
-			obj.methods[k]();
-		} while (true);
-		
-		return 0;
-	}
+		k = obj.menu();
+		if (k == 0) break;
+		obj.doit(k);
+	} while (true);
+	return 0;
+}
--- Lab1/lab1.h
+++ Lab1/lab1.h
@@ -5,30 +5,29 @@
 #include <iostream>
 
 
+//typedef void (Rand_gen::*func)(void);
 
-//was:typedef void func(void); 
-//made:typedef void (*func)(void); 
+class Rand_gen
+{
+public:
 
-	class Rand_gen 
-	{
-		public:
-			
-			//(*) - do these;
-			int c[10]{};
-			func methods[10];
+	//(*) - do these;
+	int c[10]{};
+	func methods[10];
 
-			Rand_gen();
-			void meth01(void); //(*) //Лінійний конгруентний метод
-			void meth02(void); //(*) //Квадратичний конгруентний метод
-			void meth03(void); //(*) //Числа Фібоначчі
-			void meth04(void); //(*) //Обернена конгруентна послідовність
-			void meth05(void); //Метод об’єднання
-			void meth06(void); //Правило “3 сігма”
-			void meth07(void); //Метод полярних координат
-			void meth08(void); //Метод співвідношень
-			void meth09(void); //Метод логарифму для генерування показового розподілу
-			void meth10(void); //Метод Аренса для генерування гамма-розподілу порядку a > 1
-			int menu(void);//a menu
-	};
+	//Rand_gen();
+	void meth01(void); //(*) //Лінійний конгруентний метод
+	void meth02(void); //(*) //Квадратичний конгруентний метод
+	void meth03(void); //(*) //Числа Фібоначчі
+	void meth04(void); //(*) //Обернена конгруентна послідовність
+	void meth05(void); //Метод об’єднання
+	void meth06(void); //Правило “3 сігма”
+	void meth07(void); //Метод полярних координат
+	void meth08(void); //Метод співвідношень
+	void meth09(void); //Метод логарифму для генерування показового розподілу
+	void meth10(void); //Метод Аренса для генерування гамма-розподілу порядку a > 1
+	int menu(void);//a menu
+	//and maybe you have to add init function 
+	void doit(int k);
+};
 
-	
